378017a35d2dee63cee6ca2b6ac09c9def716fd2
EduardoGladzik <eduardogladzik@gmail.com>
2020-04-28 17:30:45 -0300

    projectdnd removido

--- projectdnd/src/main/java/gladzik/eduardo/projectdnd/respository/SpellsRepository.java
+++
@@ -1,8 +0,0 @@
-package gladzik.eduardo.projectdnd.respository;
-
-import gladzik.eduardo.projectdnd.model.SpellsEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface SpellsRepository extends CrudRepository<SpellsEntity, Long> {
-}
--- projectdnd/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into spells(id_spell, name, range, duration, spell_level, classes, casting)
-    values(null, 'Acid Arrow', 90, 'instantaneous', 2, 'Wizard', '1 action');
-
-insert into spells(id_spell, name, range, duration, spell_level, classes, casting)
-    values(null, 'Acid Splash', 60, 'instantaneous', 0, 'Sorcerer', '1 acrion');
-
